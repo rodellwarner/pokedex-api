b573a25f2922b2f8b9edc02e8f4b8a7cfb300901
Rodell Warner <rodell.warner@gmail.com>
2021-04-09 08:34:13 -0500

    types and pokemon endpoints set up

--- server.js
+++ server.js
@@ -1,13 +1,45 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+
+console.log(process.env.API_TOKEN);
 
 const app = express();
 
 app.use(morgan("dev"));
 
-app.use((req, res) => {
-  res.send("Hello world!");
-});
+const validTypes = [
+  `Bug`,
+  `Dark`,
+  `Dragon`,
+  `Electric`,
+  `Fairy`,
+  `Fighting`,
+  `Fire`,
+  `Flying`,
+  `Ghost`,
+  `Grass`,
+  `Ground`,
+  `Ice`,
+  `Normal`,
+  `Poison`,
+  `Psychic`,
+  `Rock`,
+  `Steel`,
+  `Water`,
+];
+
+function handleGetTypes(req, res) {
+  res.json(validTypes);
+}
+
+function handleGetPokemon(req, res) {
+  res.send("Hello, Pokemon!");
+}
+
+app.get("/types", handleGetTypes);
+
+app.get("/pokemon", handleGetPokemon);
 
 const PORT = 8000;
 
